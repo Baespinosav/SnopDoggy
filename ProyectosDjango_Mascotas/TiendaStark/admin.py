c4552ea9ecb6210d8220c8178f0496903c831eda
AfroVin <102440210+AfroVin@users.noreply.github.com>
2022-06-14 19:56:52 -0400

    Delete admin.py

--- ProyectosDjango_Mascotas/TiendaStark/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Categoria, manProducto, Vehiculo, categoriaProducto
-
-# Register your models here.
-
-admin.site.register(Categoria)
-admin.site.register(Vehiculo)
-admin.site.register(categoriaProducto)
-admin.site.register(manProducto)
